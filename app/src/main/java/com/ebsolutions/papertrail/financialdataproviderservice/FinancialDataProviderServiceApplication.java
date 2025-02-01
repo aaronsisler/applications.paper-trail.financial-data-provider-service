8ae7689e27a9e7c4d26768935db5c72bcfa66a0c
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-01-31 21:28:47 -0500

    Pushing changes for Maven checkstyle and suppression of Javadoc only

--- app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/FinancialDataProviderServiceApplication.java
+++ app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/FinancialDataProviderServiceApplication.java
@@ -1,13 +1,8 @@
 package com.ebsolutions.papertrail.financialdataproviderservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SuppressWarnings("checkstyle:MissingJavadocType")
-@SpringBootApplication
 public class FinancialDataProviderServiceApplication {
-
-  @SuppressWarnings("checkstyle:MissingJavadocMethod")
   public static void main(String[] args) {
     SpringApplication.run(FinancialDataProviderServiceApplication.class, args);
   }
