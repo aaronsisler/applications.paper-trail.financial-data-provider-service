0ba2ae54e794011f26a0343fcd028085b6b10249
Aaron Sisler <aaronsisler@users.noreply.github.com>
2025-01-31 21:29:34 -0500

    Feature/base framework (#1)
    * Base commit for safe keeping
    
    * Pushing changes for Maven checkstyle and suppression of Javadoc only

---
+++ app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/FinancialDataProviderServiceApplication.java
@@ -0,0 +1,9 @@
+package com.ebsolutions.papertrail.financialdataproviderservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class FinancialDataProviderServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FinancialDataProviderServiceApplication.class, args);
+  }
+}
