c95fb10fb656eafac64879121d57dd4f0b3db6a9
Aaron Sisler <aaronsisler@users.noreply.github.com>
2025-02-04 06:30:21 -0500

    Feature/cucumber testing (#3)
    * Adding in the pieces for Cucumber and Spring testing. Including the /actuator/info endpoint test
    
    * Adding in VB and CL change

--- app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/FinancialDataProviderServiceApplication.java
+++
@@ -1,9 +0,0 @@
-package com.ebsolutions.papertrail.financialdataproviderservice;
-
-import org.springframework.boot.SpringApplication;
-
-public class FinancialDataProviderServiceApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(FinancialDataProviderServiceApplication.class, args);
-  }
-}
---
+++ app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/config/Constants.java
@@ -0,0 +1,7 @@
+package com.ebsolutions.papertrail.financialdataproviderservice.config;
+
+public class Constants {
+  // URLs
+  public static final String HEALTH_CHECK_URL = "/actuator/health";
+  public static final String INFO_CHECK_URL = "/actuator/info";
+}
