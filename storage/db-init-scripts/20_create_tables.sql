d0529c3fd8a55c4e8e23c44989c8003a97820be8
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-02-08 21:17:45 -0500

    Adding in the base for the DB and about to try and read_users from application

---
+++ storage/db-init-scripts/20_create_tables.sql
@@ -0,0 +1,51 @@
+/*
+* A user of the application
+*/
+CREATE TABLE IF NOT EXISTS app_user (
+  user_id integer NOT NULL,
+  username varchar(45) NOT NULL,
+  first_name varchar(45) NOT NULL,
+  last_name varchar(45) NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
+/*
+* A financial institution
+*/
+CREATE TABLE IF NOT EXISTS institution (
+  institution_id integer NOT NULL,
+  name varchar(45) NOT NULL,
+  PRIMARY KEY (institution_id)
+);
+
+/*
+* A user’s association with an institution
+*/
+CREATE TABLE IF NOT EXISTS member (
+  member_id integer NOT NULL,
+  PRIMARY KEY (member_id),
+  user_id integer REFERENCES app_user (user_id),
+  institution_id integer REFERENCES institution (institution_id)
+);
+
+/*
+* An account held at an institution which is tied to a member
+*/
+CREATE TABLE IF NOT EXISTS account (
+  account_id integer NOT NULL,
+  name varchar(45) NOT NULL,
+  PRIMARY KEY (account_id),
+  member_id integer REFERENCES member (member_id),
+  institution_id integer REFERENCES institution (institution_id)
+);
+
+/*
+* An instance of money that moves in or out of an account
+*/
+CREATE TABLE IF NOT EXISTS act_transaction (
+  act_transaction_id integer NOT NULL,
+  amount numeric(12,2) NOT NULL,
+  description varchar(150) NOT NULL,
+  PRIMARY KEY (act_transaction_id),
+  account_id integer REFERENCES account (account_id)
+);
---
+++ storage/db-init-scripts/21_insert_base_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO app_user (user_id, username, first_name, last_name)
+VALUES
+(1, 'aaron_sisler', 'Aaron', 'Sisler'),
+(2, 'bridget_sisler', 'Bridget', 'Sisler');
