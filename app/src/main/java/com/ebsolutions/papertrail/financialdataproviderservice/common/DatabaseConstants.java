36fefcaaf431a3d42cc01805303285c4e91b9232
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-02-10 20:33:13 -0500

    Working example of JDBC. Need to write tests to try working with mocking that layer

---
+++ app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/common/DatabaseConstants.java
@@ -0,0 +1,7 @@
+package com.ebsolutions.papertrail.financialdataproviderservice.common;
+
+public class DatabaseConstants {
+  public static final String DB_URL = "jdbc:postgresql://localhost:5432/paper_trail";
+  public static final String DB_USER = "admin";
+  public static final String DB_PASSWORD = "root";
+}
