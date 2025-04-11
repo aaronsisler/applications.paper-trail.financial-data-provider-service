fb28260ac57ea14fc25a287e6f8cedf21af957e1
Aaron Sisler <aaronsisler@users.noreply.github.com>
2025-04-11 12:46:08 -0400

    Feature/fix ide (#24)
    * Adding in .iml to gitignore
    
    * Removing the stand alone config and moving to base test for now
    
    * MVB and CL

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * `Fixed` for any bug fixes.
 * `Security` in case of vulnerabilities.
 
+## [0.18.0] Testing in IDE
+
+### Changed
+
+- Moved the TestConfiguration from a standalone class to a static inner class of the BaseTest
+
 ## [0.17.0] Account Transaction Create/Get/Get All/Update/Delete Integration Tests
 
 ### Added
--- app/src/test/java/com/ebsolutions/papertrail/financialdataproviderservice/tooling/BaseTest.java
+++ app/src/test/java/com/ebsolutions/papertrail/financialdataproviderservice/tooling/BaseTest.java
@@ -1,20 +1,46 @@
 package com.ebsolutions.papertrail.financialdataproviderservice.tooling;
 
+import com.ebsolutions.papertrail.financialdataproviderservice.account.AccountRepository;
+import com.ebsolutions.papertrail.financialdataproviderservice.accounttransaction.AccountTransactionRepository;
+import com.ebsolutions.papertrail.financialdataproviderservice.household.HouseholdRepository;
+import com.ebsolutions.papertrail.financialdataproviderservice.householdmember.HouseholdMemberRepository;
+import com.ebsolutions.papertrail.financialdataproviderservice.institution.InstitutionRepository;
+import com.ebsolutions.papertrail.financialdataproviderservice.user.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @CucumberContextConfiguration
-@Import(MockedIntegrationConfig.class)
 public class BaseTest {
   protected final ObjectMapper objectMapper = new ObjectMapper();
 
   @Autowired
   protected MockMvc mockMvc;
+
+  @TestConfiguration
+  @SuppressWarnings("removal")
+  /*
+   * Pulling this into BaseTest since there were issues when it was a separate file
+   */
+  public static class BaseTestConfiguration {
+    @MockBean
+    protected AccountRepository accountRepository;
+    @MockBean
+    protected HouseholdMemberRepository householdMemberRepository;
+    @MockBean
+    protected HouseholdRepository householdRepository;
+    @MockBean
+    protected InstitutionRepository institutionRepository;
+    @MockBean
+    protected AccountTransactionRepository accountTransactionRepository;
+    @MockBean
+    protected UserRepository userRepository;
+  }
 }
