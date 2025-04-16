08cb3d9cde07a37d6facb95752f9a106bece9cc5
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-04-15 23:05:41 -0400

    Working ingestion of account transactions from queue. Need tests now

---
+++ app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/accounttransaction/AccountTransactionSubscriber.java
@@ -0,0 +1,83 @@
+package com.ebsolutions.papertrail.financialdataproviderservice.accounttransaction;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequest;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequestEntry;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+@EnableScheduling
+@EnableAsync
+public class AccountTransactionSubscriber {
+  private final SqsClient sqsClient;
+  private final AccountTransactionQueue accountTransactionQueue;
+  private final ObjectMapper objectMapper;
+  private final AccountTransactionService accountTransactionService;
+
+
+  @Async
+  @Scheduled(fixedRate = 1000, initialDelay = 1000)
+  public void consumeMessages() throws JsonProcessingException {
+    ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+        .queueUrl(accountTransactionQueue.getQueueUrl())
+        .maxNumberOfMessages(10)
+        .waitTimeSeconds(2)
+        .build();
+
+
+    List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+
+    // Exit the loop if no messages on the queue
+    if (messages.isEmpty()) {
+      return;
+    }
+
+    List<DeleteMessageBatchRequestEntry> deleteMessageBatchRequestEntries = new ArrayList<>();
+    List<AccountTransaction> accountTransactions = new ArrayList<>();
+
+    for (Message message : messages) {
+      System.out.println("Thread: " + Thread.currentThread().threadId());
+      System.out.println("Message");
+      System.out.println(message.body());
+      AccountTransaction accountTransaction =
+          objectMapper.readValue(message.body(), AccountTransaction.class);
+      accountTransactions.add(accountTransaction);
+
+      deleteMessageBatchRequestEntries.add(
+          DeleteMessageBatchRequestEntry.builder()
+              .id(message.messageId())
+              .receiptHandle(message.receiptHandle())
+              .build()
+      );
+    }
+
+    try {
+      accountTransactionService.createAll(accountTransactions);
+    } catch (Exception exception) {
+      log.error("Cannot save account transactions to data store");
+      log.error(exception.getMessage());
+    }
+
+    DeleteMessageBatchRequest deleteMessageBatchRequest =
+        DeleteMessageBatchRequest.builder().queueUrl(accountTransactionQueue.getQueueUrl())
+            .entries(deleteMessageBatchRequestEntries)
+            .build();
+
+    sqsClient.deleteMessageBatch(deleteMessageBatchRequest);
+  }
+}
---
+++ app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/config/QueueConfig.java
@@ -0,0 +1,48 @@
+package com.ebsolutions.papertrail.financialdataproviderservice.config;
+
+import com.ebsolutions.papertrail.financialdataproviderservice.accounttransaction.AccountTransactionQueue;
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+
+@Configuration
+public class QueueConfig {
+  @Value("${infrastructure.endpoint:`Infrastructure endpoint not found in environment`}")
+  protected String endpoint;
+
+  @Value("${infrastructure.messaging.queue-url:`Queue name not found in environment`}")
+  protected String queueUrl;
+
+  @Bean
+  @Profile({"local"})
+  public SqsClient localSqsClientInstantiation() {
+    return SqsClient.builder()
+        .region(Region.US_EAST_1)
+        .endpointOverride(URI.create(endpoint))
+        .credentialsProvider(staticCredentialsProvider())
+        .build();
+  }
+
+  @Bean
+  @Profile({"local"})
+  public AccountTransactionQueue accountTransactionQueue() {
+    return AccountTransactionQueue.builder()
+        .queueUrl(queueUrl)
+        .build();
+  }
+
+  public StaticCredentialsProvider staticCredentialsProvider() {
+    String awsAccessKeyId = "accessKeyId";
+    String awsSecretAccessKey = "secretAccessKey";
+
+    return StaticCredentialsProvider.create(
+        AwsBasicCredentials.create(awsAccessKeyId, awsSecretAccessKey));
+  }
+}
