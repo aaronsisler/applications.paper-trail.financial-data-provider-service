782e12d4774db00c844fe257fd7e4676e76b7cfe
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-04-17 18:07:24 -0400

    Holding so I can change the fact the a request can't have differing account ids

--- app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/accounttransaction/AccountTransactionQueueSubscriber.java
+++ app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/accounttransaction/AccountTransactionQueueSubscriber.java
@@ -1,11 +1,14 @@
 package com.ebsolutions.papertrail.financialdataproviderservice.accounttransaction;
 
+import com.ebsolutions.papertrail.financialdataproviderservice.common.exception.DataConstraintException;
+import com.ebsolutions.papertrail.financialdataproviderservice.common.exception.DataProcessingException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -23,7 +26,7 @@ import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 @RequiredArgsConstructor
 @EnableScheduling
 @EnableAsync
-public class AccountTransactionSubscriber {
+public class AccountTransactionQueueSubscriber {
   private final SqsClient sqsClient;
   private final AccountTransactionQueue accountTransactionQueue;
   private final ObjectMapper objectMapper;
@@ -33,14 +36,21 @@ public class AccountTransactionSubscriber {
   @Async
   @Scheduled(fixedRate = 1000, initialDelay = 1000)
   public void consumeMessages() throws JsonProcessingException {
-    ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
-        .queueUrl(accountTransactionQueue.getQueueUrl())
-        .maxNumberOfMessages(10)
-        .waitTimeSeconds(2)
-        .build();
+    List<Message> messages;
 
+    try {
+      ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+          .queueUrl(accountTransactionQueue.getQueueUrl())
+          .maxNumberOfMessages(10)
+          .waitTimeSeconds(2)
+          .build();
 
-    List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+      messages = new ArrayList<>(sqsClient.receiveMessage(receiveMessageRequest).messages());
+    } catch (Exception exception) {
+      log.error("Cannot retrieve messages from the queue");
+      log.error(exception.getMessage());
+      return;
+    }
 
     // Exit the loop if no messages on the queue
     if (messages.isEmpty()) {
@@ -67,16 +77,32 @@ public class AccountTransactionSubscriber {
 
     try {
       accountTransactionService.createAll(accountTransactions);
+    } catch (DataConstraintException dataConstraintException) {
+      log.error("There is a data constraint issue");
+      return;
+    } catch (DataIntegrityViolationException dataIntegrityViolationException) {
+      log.error("There is a data issue with a missing account");
+      return;
+    } catch (DataProcessingException dataProcessingException) {
+      log.error("Cannot save account transactions to data store - DataProcessingException");
+      log.error(dataProcessingException.getMessage());
+      return;
     } catch (Exception exception) {
-      log.error("Cannot save account transactions to data store");
+      log.error("Cannot save account transactions to data store - Exception");
       log.error(exception.getMessage());
+      return;
     }
 
-    DeleteMessageBatchRequest deleteMessageBatchRequest =
-        DeleteMessageBatchRequest.builder().queueUrl(accountTransactionQueue.getQueueUrl())
-            .entries(deleteMessageBatchRequestEntries)
-            .build();
+    try {
+      DeleteMessageBatchRequest deleteMessageBatchRequest =
+          DeleteMessageBatchRequest.builder().queueUrl(accountTransactionQueue.getQueueUrl())
+              .entries(deleteMessageBatchRequestEntries)
+              .build();
 
-    sqsClient.deleteMessageBatch(deleteMessageBatchRequest);
+      sqsClient.deleteMessageBatch(deleteMessageBatchRequest);
+    } catch (Exception exception) {
+      log.error("Cannot delete account transactions from the queue");
+      log.error(exception.getMessage());
+    }
   }
 }
