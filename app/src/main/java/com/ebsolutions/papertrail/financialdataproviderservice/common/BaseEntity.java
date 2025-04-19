d974c0963095dc860e9872f602332da3bbaabfeb
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-04-19 06:20:16 -0400

    Removing the BaseEntity's Data annotation for Getter/Setter

--- app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/common/BaseEntity.java
+++ app/src/main/java/com/ebsolutions/papertrail/financialdataproviderservice/common/BaseEntity.java
@@ -4,11 +4,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-@Data
+@Getter
+@Setter
 @MappedSuperclass
 @SuperBuilder
 @RequiredArgsConstructor
